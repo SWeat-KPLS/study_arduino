b6834227bf49772cee4f04aad483130d492ba5d2
whoonypark <whoony101@gmail.com>
2023-01-13 22:12:11 +0900

    ch.3 p.42 LED 1개 제어

--- Park/main.py
+++ Park/main.py
@@ -1,2 +1,11 @@
-print("Hello world!")
-print("hello")
+from Arduino import Arduino
+import time
+
+board = Arduino('9600')
+board.pinMode(13, "OUTPUT")
+
+while True:
+    board.digitalWrite(13, "LOW")
+    time.sleep(1)
+    board.digitalWrite(13, "HIGH")
+    time.sleep(1) 
