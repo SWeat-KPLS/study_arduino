898093b919e23e1e9daec12e97059b1c65c5e5ab
ImJeongHoon <ijh701016@naver.com>
2023-01-27 20:38:12 +0900

    11부터 14까지

--- Lim/main.py
+++ Lim/main.py
@@ -1,15 +1,13 @@
 from Arduino import Arduino
 import time
 
-BLED, LIGHT = 9, 0 # const
-val = 0
+LED_PIN = 13
 
-board = Arduino('9600')
-board.pinMode(BLED, 'OUTPUT') # LED
-board.pinMode(LIGHT, 'INPUT') # sensor
+board = Arduino('9600', port='COM13') # 알맞는 port를 전달해주세요
+board.pinMode(LED_PIN, 'OUTPUT')
 
-while True :
-    val = board.analogRead(LIGHT)
-    print(val)
-    board.analogWrite(BLED, int((val*255)/600))
-    time.sleep(0.05)
+while True:
+    board.digitalWrite(LED_PIN, 'LOW')
+    time.sleep(1)
+    board.digitalWrite(LED_PIN, 'HIGH')
+    time.sleep(1)
